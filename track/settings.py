23f5f75c9596f4ef8494c7d1f6622b47188bba6d
Andrey Miskov <amiskov@gmail.com>
2021-11-30 12:27:58 +0300

    Remove static dirs

--- track/settings.py
+++ track/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    '/home/amiskov/track/static',  # PythonAnywhere app
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
